41e2fd7006b5c7d2022ae44a9dd78b8a815e0d06
SarahTheodoro <133895051+SarahTheodoro@users.noreply.github.com>
2023-10-18 07:53:32 -0300

    Update main.js

--- aula invertida site/main.js
+++ aula invertida site/main.js
@@ -2,14 +2,12 @@
 function inicio() {
     const texto = document.querySelector("h1");
     const descri = document.querySelector("p");
-    const descric = document.querySelector("p");
     const imagem = document.querySelector("img");
-    const texto1 = document.querySelector("h2");
     const valor = document.querySelector('#valor').value;
    
     if((valor=="Objeto") || (valor=="objeto") || (valor=="OBJETO")){
         texto.innerHTML = "Objeto";
-        descri.innerHTML = " Uma entidade que possui estado e comportamento é conhecida como um objeto, por exemplo, cadeira, bicicleta, marcador, caneta, mesa, carro, etc. Pode ser física ou lógica (tangível e intangível). O exemplo de um objeto intangível é o sistema bancário. Por exemplo, caneta é um objeto. O nome é Reynolds; a cor é branca, são conhecidos como estado. É usado para escrever, então escrever é o seu comportamento. Um objeto é uma instância de uma classe. Uma classe é um modelo ou blueprint a partir do qual os objetos são criados. Portanto, um objeto é a instância (resultado) de uma classe. Definições de objeto: Um objeto é uma entidade do mundo real. Um objeto é uma entidade de tempo de execução. O objeto é uma entidade que possui estado e comportamento. LINK: https://www.alura.com.br/artigos/como-comparar-objetos-em-java                                                                                                         ";                       
+        descri.innerHTML = " Uma entidade que possui estado e comportamento é conhecida como um objeto, por exemplo, cadeira, bicicleta, marcador, caneta, mesa, carro, etc. Pode ser física ou lógica (tangível e intangível). O exemplo de um objeto intangível é o sistema bancário. Por exemplo, caneta é um objeto. O nome é Reynolds; a cor é branca, são conhecidos como estado. É usado para escrever, então escrever é o seu comportamento. Um objeto é uma instância de uma classe. Uma classe é um modelo ou blueprint a partir do qual os objetos são criados. Portanto, um objeto é a instância (resultado) de uma classe. Definições de objeto: Um objeto é uma entidade do mundo real. Um objeto é uma entidade de tempo de execução. O objeto é uma entidade que possui estado e comportamento. LINK: https://www.alura.com.br/artigos/como-comparar-objetos-em-java";                       
         imagem.setAttribute("src","img/objeto.png");
         imagem.setAttribute("width","300px");
      
@@ -18,7 +16,7 @@ function inicio() {
 
     else if((valor=="Classe") || (valor=="classe") || (valor=="CLASSE")){
         texto.innerHTML = "Classe"; 
-        descri.innerHTML = "CLASSES: Uma classe em programação orientada a objetos (POO) é uma estrutura que define o comportamento e as características de um objeto. Ela serve como um modelo ou plano para criar objetos. Em resumo, uma classe contém: 1. Atributos 2. Métodos 3. Construtores 4. Membros 5. Herança 6. Encapsulamento 7. Abstração 8. Polimorfismo. Uma classe serve como um "modelo" a partir do qual objetos podem ser instanciados. É uma maneira de organizar e estruturar código, promovendo a reutilização e a manutenção do software."
+        descri.innerHTML = "CLASSES: Uma classe em programação orientada a objetos (POO) é uma estrutura que define o comportamento e as características de um objeto. Ela serve como um modelo ou plano para criar objetos. Em resumo, uma classe contém: 1. Atributos 2. Métodos 3. Construtores 4. Membros 5. Herança 6. Encapsulamento 7. Abstração 8. Polimorfismo. Uma classe serve como um modelo a partir do qual objetos podem ser instanciados. É uma maneira de organizar e estruturar código, promovendo a reutilização e a manutenção do software. LINK: https://www.alura.com.br/artigos/como-comparar-objetos-em-java";
         imagem.setAttribute("src","img/classe.webp");
         imagem.setAttribute("width","300px");
       }
